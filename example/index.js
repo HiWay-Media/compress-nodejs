ab1db282593fc09061702f4a3fce42a22c469cd9
Allan Nava <allannava95@gmail.com>
2023-10-12 16:03:03 +0200

    added index

---
+++ example/index.js
@@ -0,0 +1,16 @@
+/*
+ *
+ * Author: Allan Nava (allan.nava@hiway.media)
+ * -----
+ * Last Modified: 
+ * Modified By: Allan Nava (allan.nava@hiway.media>)
+ * -----
+ * Copyright 2023 - 2023 © 
+ * 
+ */
+import { TangramClient } from "compress-nodejs";
+
+let tngrm = new TangramClient(
+    "api_key",
+    "customer"
+);

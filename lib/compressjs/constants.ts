6c87224299f0d885c8616e06c56a3aaa256b0a66
Allan Nava <allannava95@gmail.com>
2024-06-03 10:09:48 +0200

    added new endpooints

--- lib/compressjs/constants.ts
+++ lib/compressjs/constants.ts
@@ -1,22 +1,35 @@
 /*
  *
- * Author: Allan Nava   (allan.nava@hiway.media)
- * Author: Daniel Botta (daniel.botta@hiway.media)
+ * Author: Allan Nava       (allan.nava@hiway.media)
+ * Author: Daniel Botta     (daniel.botta@hiway.media)
+ * Author: Antonio Borgese  (antonio.borgese.hiway.media) 
  * -----
  * Last Modified: 
- * Modified By: Allan Nava (allan.nava@hiway.media>)
+ * Modified By: Allan Nava  (allan.nava@hiway.media>)
  * -----
  * Copyright 2023 - 2024 © 
  * 
  */
 
 export const TNGRM_BASE_URL                 = "https://api-compress.hiway.media/api/v4.0";
-export const AUTH_LOGIN                     = "/external/auth/login";
+//export const AUTH_LOGIN                     = "/external/auth/login"; deprecated
 export const GET_CATEGORIES                 = "/external/upload/categories";
 export const CREATE_CATEGORY                = `${GET_CATEGORIES}/create`;
+// restreamers 
 export const GET_RESTREAMERS                = "/external/restreamers";
 export const GET_RUNNING_INSTANCES          = `${GET_RESTREAMERS}/running_instances`;
 export const GET_RUNNING_SINGLE_INSTANCE    = `${GET_RESTREAMERS}/single_instance`;
+export const SCALE_RESTREAMER               = `${GET_RESTREAMERS}/scale_instance`;
+export const RESTREAMER_HLS_START           = `${GET_RESTREAMERS}/hls/start`;
+export const RESTREAMER_HLS_STOP            = `${GET_RESTREAMERS}/hls/stop`;
+export const RESTREAMER_PUSH_START          = `${GET_RESTREAMERS}/push/start`;
+export const RESTREAMER_PUSH_STOP           = `${GET_RESTREAMERS}/push/stop`;
+export const RESTREAMER_PULL_START          = `${GET_RESTREAMERS}/pull/start`;
+export const RESTREAMER_PULL_STOP           = `${GET_RESTREAMERS}/pull/stop`;
+// events 
+export const EVENTS_EXTERNAL                = "/external/events";
+export const BULK_EVENTS_CREATE             = `${EVENTS_EXTERNAL}/create_bulk`;
+// video upload
 export const GET_UPLOADS                    = "/external/upload";
 export const GET_SINGLE_UPLOAD              = `${GET_UPLOADS}/jobid`;
 export const GET_JOBID_PROGRESS             = `${GET_UPLOADS}/job_progress/jobid`;
@@ -26,7 +39,6 @@ export const PRESIGNED_URL_S3               = `${GET_UPLOADS}/presignedUrl`;
 export const S3_SPACE                       = `${GET_UPLOADS}/s3_space`;
 export const ADD_VIDEO_THUMB                = `${GET_UPLOADS}/add_thumb`;
 export const SIGN_S3_URL                    = `${GET_UPLOADS}/sign_s3_url`;
-export const SCALE_RESTREAMER               = `${GET_RESTREAMERS}/scale_instance`;
 export const CREDENTIALS                    = `/external/credentials/`;
 export const PERMITTED_FILE_EXTENSIONS = [
   ".mov",

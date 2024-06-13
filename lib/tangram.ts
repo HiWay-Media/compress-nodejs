a0258f646a4a4be2e3f34b3200d03dca6ed37fa2
Allan Nava <allannava95@gmail.com>
2024-06-13 12:09:55 +0200

    added get_restreamers_ott_all

--- lib/tangram.ts
+++ lib/tangram.ts
@@ -448,6 +448,35 @@ class TangramClient {
 
   /**
    * 
+   * @param {number} start_from 
+   * @param {number} amount 
+   * @returns restreamer list
+   */
+  async get_restreamers_ott_all(start_from, amount) {
+    return await fetch(TNGRM_BASE_URL + GET_RESTREAMERS_OTT_ALL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        api_key: this.api_key,
+        client_id: this.client_id,
+        start_from: parseInt(start_from),
+        amount: parseInt(amount)
+      }),
+    })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(
+          `something went wrong during get restreamers, ${res.status} ${res.statusText}`
+        );
+      }
+      return res.json();
+    })
+  }
+
+  /**
+   * 
    * @returns restreamer object
    */
   async get_restreamer(instance_name) {
